84af77a047bc2965fbc562703c581c5561e23f47
jkimball54 <jeremyck54@gmail.com>
2023-07-26 14:56:37 -0600

    Index & Show Complete

---
+++ CrudBucketMVC/Controllers/OwnersController.cs
@@ -0,0 +1,27 @@
+﻿using CrudBucketMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudBucketMVC.Controllers
+{
+    public class OwnersController : Controller
+    {
+        private readonly CrudBucketContext _context;
+
+        public OwnersController(CrudBucketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var owners = _context.Owners;
+            return View(owners);
+        }
+        [Route("/Owners/{id:int}")]
+        public IActionResult Show(int id)
+        {
+            var owner = _context.Owners.Find(id);
+            return View(owner);
+        }
+    }
+}
